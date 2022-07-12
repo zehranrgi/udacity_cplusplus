bf6ffe84ac492c105b7a80acdfb49878a3f1ebe9
Zehra <70450368+zehranrgi@users.noreply.github.com>
2022-07-12 12:00:22 +0300

    Create polymorphism_lesson12.cpp

---
+++ polymorphism_lesson12.cpp
@@ -0,0 +1,13 @@
+//Note-1:
+//Good explanation on that: 
+// https://www.youtube.com/watch?v=wK93q-JQUvw
+
+/*. A real-life example of polymorphism is a person who at the same time can have different characteristics. 
+Like a man at the same time is a father, a husband and an employee.
+So the same person exhibits different behavior in different situations. This is called polymorphism. (Source: GG)
+
+Example in this page is very clear: 
+
+https://www.geeksforgeeks.org/polymorphism-in-c/ 
+
+*/ 
